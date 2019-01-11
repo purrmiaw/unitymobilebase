e97fd73e8e094ce00ddadae4fa7741c1ec72d395
PURR\Purr <purr@miaw.xyz>
2019-01-12 01:52:07 +0800

    admob
    admob

---
+++ Assets/_Game/Scripts/AdmobBannerScript.cs
@@ -0,0 +1,69 @@
+﻿using GoogleMobileAds.Api;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdmobBannerScript : MonoBehaviour
+{
+
+    private BannerView _bannerView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        string appId = "";
+
+#if UNITY_ANDROID
+            appId = "ca-app-pub-3940256099942544~3347511713";
+#elif UNITY_IPHONE
+            appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+        appId = "unexpected_platform";
+#endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        _bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+        _bannerView.OnAdLoaded += HandleOnAdLoaded;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+
+
+        // Load the banner with the request.
+        _bannerView.LoadAd(request);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        _bannerView.Show();
+    }
+
+}
---
+++ Assets/_Game/Scripts/AdmobInterstitialScript.cs
@@ -0,0 +1,52 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdmobInterstitialScript : MonoBehaviour
+{
+
+    private InterstitialAd _interstitial;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RequestInterstitial();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("GameOver", 5);
+    }
+
+    private void RequestInterstitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        _interstitial = new InterstitialAd(adUnitId);
+
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        _interstitial.LoadAd(request);
+    }
+
+    private void GameOver()
+    {
+        if (_interstitial.IsLoaded())
+        {
+            _interstitial.Show();
+        }
+    }
+
+}
