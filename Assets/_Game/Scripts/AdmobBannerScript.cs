7f888afa226a0f44ab85b6aaa5caf2d79528f99c
PURR\Purr <purr@miaw.xyz>
2019-04-22 18:44:47 +0800

    Now can change notification icon for Android

--- Assets/_Game/Scripts/AdmobBannerScript.cs
+++ Assets/_Game/Scripts/AdmobBannerScript.cs
@@ -1,36 +1,26 @@
-﻿using GoogleMobileAds.Api;
+﻿using Assets._Game.Scripts;
+using GoogleMobileAds.Api;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AdmobBannerScript : MonoBehaviour
+public class AdmobBannerScript : BaseMonoBehaviour
 {
-
     private BannerView _bannerView;
 
     // Start is called before the first frame update zz
     void Start()
     {
-
-        string appId = "";
-
-#if UNITY_ANDROID
-            appId = "ca-app-pub-5129348250377085~1856868770";
-#elif UNITY_IPHONE
-            appId = "ca-app-pub-3940256099942544~1458002511";
-#else
-        appId = "unexpected_platform";
-#endif
-
         // Initialize the Google Mobile Ads SDK.
-        MobileAds.Initialize(appId);
+        MobileAds.Initialize(_appId);
 
         RequestBanner();
     }
 
     private void RequestBanner()
     {
+
 #if UNITY_ANDROID
             string adUnitId = "ca-app-pub-3940256099942544/6300978111";
 #elif UNITY_IPHONE
@@ -47,12 +37,8 @@ public class AdmobBannerScript : MonoBehaviour
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().Build();
 
-
-
         // Load the banner with the request.
         _bannerView.LoadAd(request);
-
-
     }
 
     // Update is called once per frame
---
+++ Assets/_Game/Scripts/BaseMonoBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Game.Scripts
+{
+    public class BaseMonoBehaviour : MonoBehaviour
+    {
+        protected string _appId {
+            get
+            {
+                string appId = "";
+
+#if UNITY_ANDROID
+                appId = "ca-app-pub-5129348250377085~1856868770";
+#elif UNITY_IPHONE
+            appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+        appId = "unexpected_platform";
+#endif
+
+                return appId;
+            }
+        }
+    }
+}
